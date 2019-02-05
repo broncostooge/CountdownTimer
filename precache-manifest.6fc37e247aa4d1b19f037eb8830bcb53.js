d7e56e843bb2e19440438a952e050b7cf9873315
Stephan Jones <sjones.csuf@gmail.com>
2019-02-04 18:58:54 -0800

    Updates

---
+++ precache-manifest.6fc37e247aa4d1b19f037eb8830bcb53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47249288ecba58e52d5b",
+    "url": "/countdown/static/css/main.5d5dae7c.chunk.css"
+  },
+  {
+    "revision": "47249288ecba58e52d5b",
+    "url": "/countdown/static/js/main.47249288.chunk.js"
+  },
+  {
+    "revision": "0ac6f8653c9cb3da911a",
+    "url": "/countdown/static/css/1.534d00fa.chunk.css"
+  },
+  {
+    "revision": "0ac6f8653c9cb3da911a",
+    "url": "/countdown/static/js/1.0ac6f865.chunk.js"
+  },
+  {
+    "revision": "08800195382208a303b6",
+    "url": "/countdown/static/js/runtime~main.08800195.js"
+  },
+  {
+    "revision": "b2cee75833d2cb73d73358b1a3533198",
+    "url": "/countdown/index.html"
+  }
+];
